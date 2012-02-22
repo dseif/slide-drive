40d2faa8796c03010ddad942d394b48d6caa5944
root <root@li366-151.members.linode.com>
2012-02-22 01:19:36 -0500

    Fixed Issues on iPad, all original functionality is now available tho a bit of a startup lag

---
+++ js/mobile.js
@@ -0,0 +1,41 @@
+$(function() {
+	// Deck initialization
+  $("audio").mediaelementplayer({
+    audioWidth: document.body.offsetWidth,
+    success: function( mediaElement, domObject ) {
+      window.playerReady = true;
+    }
+  });
+$.deck('.slide');
+
+$('#style-themes').change(function() {
+$('#style-theme-link').attr('href', $(this).val());
+});
+
+$('#transition-themes').change(function() {
+$('#transition-theme-link').attr('href', $(this).val());
+});
+
+  $(document).bind('deck.change', function(event, from, to) {
+    var container = $( ".deck-container" )[ 0 ],
+        slide = $.deck( "getSlide", to )[ 0 ];
+    if( slide.offsetHeight > container.offsetHeight) {
+      container.style.overflowY = "auto";
+    } else {
+      container.style.overflow = "hidden";
+    }
+  });
+
+  var resize = function() {
+    var elem = document.getElementById( "slideshow-transcript" );
+    elem.style.height = (document.body.offsetHeight - elem.offsetTop - 3)   + "px";
+  };
+  window.addEventListener( "resize", function( e ) {
+    resize();
+  }, false);
+  window.addEventListener( "load", function( e ) {
+    resize();
+  }, false );
+
+});
+
