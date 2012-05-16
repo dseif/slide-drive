5001bb45143355cec81b97e57e357678d928652b
Jeremy Banks <jeremy@jeremybanks.ca>
2012-05-16 10:40:01 -0400

    test tweaks, switching CDN to Google for jQuery

--- test/atesting.js
+++ test/atesting.js
@@ -1,6 +1,6 @@
 /* atest.js
 
-  atest( [name,] body( [assert, pass, toCleanUp] ) );
+  atest( [name,], [timeout,] body( [toCleanUp] ) );
     Declares an test with the specifed name and body. It will be
     run syncronously with other tests defined syncronously with it.
 
@@ -19,16 +19,16 @@
   The purpose is to allow you to avoid running all child tests if
   some parent set-up has failed, but generally not need to manually
   pass/fail the parent test.
-
 */
 
 (function ( root ) {
   "use strict";
   
   root.atest = atest;
+  root.ATest = ATest;
 
-  var TEST_TIMEOUT = 60000,
-      TICK_INTERVAL = 100;
+  var TEST_TIMEOUT_S =    20,
+      TICK_INTERVAL_MS = 100;
 
   var activeParent = null;
 
@@ -38,43 +38,47 @@
     if ( activeParent ) {
       parent = activeParent;
     } else {
-      parent = new ATest( function () {}, "<test root>" );
+      parent = new ATest( function () {}, "<test root>", Infinity, 1 );
       setTimeout( parent.requested.resolve, 0 );
     }
     
-    var name,
-    body;
-
-    if ( typeof arguments[ 0 ] === "string" ) {
-      name = arguments[ 0 ];
-      body = arguments[ 1 ];
-    } else {
-      name = null;
-      body = arguments[ 0 ];
-    }
+    var args = [].slice.call( arguments );
     
-    var test = new ATest( body, name );
+    var name = ( typeof args[ 0 ] === "string" ) ? args.shift() : "<test>",
+        timeout_s = ( typeof args[ 0 ] === "number" ) ? args.shift() : TEST_TIMEOUT_S,
+        body = args.shift();
+    
+    var test = new ATest( body, name, timeout_s );
     parent.addChild( test );
     
     return test;
   }
 
-  function ATest ( body, name ) {
+  function ATest ( body, name, timeout_s ) {
     this.body = body;
     this.name = name || "<test>";
-    this.children = [],
-    this.requested = new jQuery.Deferred,
-    this.result = new jQuery.Deferred,
+    this.children = [];
+    this.requested = new jQuery.Deferred;
+    this.result = new jQuery.Deferred;
+    this.childrenCompleted = new jQuery.Deferred;
+    this.completed = new jQuery.Deferred;
+    this.outstandingChildCount = 0;
     this.report = document.createElement( "li" );
     
-    var test = this;
+    var test = this,
+        timeout = timeout_s * 1000;
+    
+    test.childrenCompleted.then( test.completed.resolve );
 
-    var ownReport = document.createElement( "div" ),
+    var ownLabel = document.createElement( "div" ),
         childReports = document.createElement( "ul" );
     
-    ownReport.className = "label";
-    ownReport.textContent = "[––––] " + test.name;
-    test.report.appendChild( ownReport );
+    ownLabel.className = "label";
+    
+    ownLabel.textContent = "[pending] " + test.name;
+    test.report.className = "pending";
+    
+    test.report.appendChild( ownLabel );
 
     test.addChild = function ( childTest ) {
       if ( test.children.length === 0 ) {
@@ -84,38 +88,59 @@
         test.requested.then( childTest.requested.resolve );
       } else {
         // Subsquent child tests will be triggered when the previous one is complete.
-        test.children[ test.children.length - 1 ].result.always( childTest.requested.resolve );
+        test.children[ test.children.length - 1 ].completed.always( childTest.requested.resolve );
       }
       
       childReports.appendChild( childTest.report );
       test.children.push( childTest );
+      
+      test.outstandingChildCount++;
+      childTest.completed.then(function () {
+        test.outstandingChildCount--;
+        
+        if (test.outstandingChildCount === 0) {
+          test.childrenCompleted.resolve();
+        }
+      })
+      
+      childTest.result.fail( function () { test.result.reject(); } );
     };
 
     test.result.then(function () {
       test.report.className = "pass";
-      ownReport.textContent = "[pass] " + test.name;
+      ownLabel.textContent = "[pass] " + test.name;
       var ownMessage = document.createElement( "div" );
       ownMessage.textContent = [].slice.call( arguments ).join( " - ");
       test.report.appendChild( ownMessage );
     }, function () {
       test.report.className = "fail";
-      ownReport.textContent = "[FAIL] " + test.name;
+      ownLabel.textContent = "[FAIL] " + test.name;
       var ownMessage = document.createElement( "div" );
       ownMessage.textContent = [].slice.call( arguments ).join( " - ");
       test.report.appendChild( ownMessage );
     })
     
-    function assert ( condition, msg ) {
-      if ( !condition ) {
-        var failArgs = [ "Assertion failed" ].concat( [].slice.call( arguments, 1 ) );
-        test.result.reject.apply( test.result, failArgs ) ;
-      }
-    }
-    
     test.requested.then(function() {
-      var tickInterval = setInterval( tick, TICK_INTERVAL ),
-          timeoutTimeout = setTimeout( test.result.reject, TEST_TIMEOUT, "Timeout (" + TEST_TIMEOUT + "ms)" ),
+      if ( test.result.state() !== "pending" ) {
+        return;
+      }
+      
+      ownLabel.textContent = "[running] " + test.name;
+      test.report.className = "running";
+      
+      var tickInterval = setInterval( tick, TICK_INTERVAL_MS ),
+          timeoutTimeout = setTimeout( test.result.reject, timeout, "Timeout (" + timeout + "ms)" ),
           active = test.body;
+      
+      if ( timeout === Infinity ) {
+        clearTimeout( timeoutTimeout );
+      }
+      
+      if ( body === true ) {
+        test.result.resolve();
+      } else if ( body === false ) {
+        test.result.reject();
+      }
       
       function tick () {
         if ( test.result.state() === "pending" && typeof active === "function" ) {
@@ -123,7 +148,7 @@
               activeParent = test;
 
           try {
-            active = active.call( test, assert, test.result.resolve, test.result.always );
+            active = active.call( test, test.result.always );
           } catch ( ex ) {
             test.result.reject( "Uncaught exception", ex );
             console.error( ex.stack );
@@ -137,16 +162,10 @@
             test.result.reject();
           }
         } else {
-          if ( test.children.length > 0 ) {
-            test.children[ test.children.length - 1 ].result.always(function () {
-              for ( var i = 0; i < test.children.length; ++i ) {
-                if ( test.children[ i ].result.state() === "rejected" ) {
-                  test.result.reject();
-                  break;
-                }
-              }
-              test.result.resolve();
-            });
+          if ( test.children.length === 0 ) {
+            test.completed.resolve();
+          } else {
+            test.childrenCompleted.then( test.result.resolve );
           }
           
           clearInterval( tickInterval );
